8277e9a22b062028dd94f76d5c91dda8282f2928
unknown <ssvrdlin@spartaglobal.com>
2019-02-13 10:02:01 +0000

    Added apt dependency

--- metadata.rb
+++ metadata.rb
@@ -7,6 +7,8 @@ long_description 'Installs/Configures uber_nginx_cookbook'
 version '0.1.0'
 chef_version '>= 13.0'
 
+depends 'apt'
+
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,6 +3,7 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+include_recipe "apt"
 package "nginx"
 
 service 'nginx' do
